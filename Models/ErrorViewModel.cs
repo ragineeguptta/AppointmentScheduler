e363edb82766970f121b0ed70bc54e854b480555
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-07-26 18:08:06 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppointmentScheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
