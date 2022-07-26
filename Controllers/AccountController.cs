ca73180f302c995b9ea891d76fdf8cdf870c79c4
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-07-26 18:30:21 +0530

    1

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using AppointmentScheduler.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduler.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
