4bf16a5cec02c7eb5e3bee1c22faa25f46408fe4
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-07-28 12:46:23 +0530

    1

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -5,9 +5,6 @@ namespace AppointmentScheduler.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-     
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -28,8 +28,7 @@ namespace AppointmentScheduler
         {
             services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
-
-            services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +48,7 @@ namespace AppointmentScheduler
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
