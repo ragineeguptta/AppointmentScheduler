e579810e0abe5d05f1c49082b9448513358eceda
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-07-28 18:49:18 +0530

    2

--- wwwroot/js/script-custom-calendar.js
+++ wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,12 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,'
+        },
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
