b9a53019fc7b7ec3d8ba0ba48d1c63ede36b37e0
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-07-29 12:45:55 +0530

    2

--- wwwroot/js/script-custom-calendar.js
+++ wwwroot/js/script-custom-calendar.js
@@ -1,14 +1,44 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay,'
-        },
-        initialView: 'dayGridMonth',
-        selectable: true,
-        editable: false
+﻿var routeURL = location.protocol + "//" + location.host;
+$(document).ready(function () {
+    $("#appointmentDate").kendoDateTimePicker({
+        value: new Date(),
+        dateInput: false
     });
-    calendar.render();
+
+    InitializeCalendar();
 });
+var calendar;
+function InitializeCalendar() {
+    try {
+
+
+        var calendarEl = document.getElementById('calendar');
+        if (calendarEl != null) {
+            calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                editable: false,
+                select: function (event) {
+                    onShowModal(event, null);
+                }
+            });
+            calendar.render();
+        }
+
+    }
+    catch (e) {
+        alert(e);
+    }
+
+}
+
+
+function onShowModal(obj, isEventDetail) {
+    $("#appointmentInput").modal("show");
+}
+
