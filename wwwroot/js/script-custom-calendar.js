7b20421680fa8a09f14f3475d6b9bba71666194a
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-07-29 18:50:13 +0530

    7

--- wwwroot/js/script-custom-calendar.js
+++ wwwroot/js/script-custom-calendar.js
@@ -46,3 +46,15 @@ function onShowModal(obj, isEventDetail) {
 function onCloseModal() {
     $("#appointmentInput").modal("hide")
 }
+
+function onSubmitForm() {
+    var requestData = {
+        Id: parseInt($("#id").val()),
+        Title: $("#title").val(),
+        Description: $("#description").val(),
+        StartDate: $("#appointmentDate").val(),
+        Duration: $("#duration").val(),
+        DoctorId: $("#doctorId").val(),
+        PatientId: $("#patientId").val(),
+    }
+}
