c800d488b74e5e9f0ae1327d56953cb313ac4292
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-08-12 18:36:16 +0530

    1

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -75,8 +75,11 @@ namespace AppointmentScheduler.Controllers
                 if (result.Succeeded)
                 {
                     await _userManager.AddToRoleAsync(user, model.RoleName);
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return RedirectToAction("Index", "Home");
+                    if (!User.IsInRole(Helper.Admin))
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                    }
+                    return RedirectToAction("Index", "Appointment");
                 }
                 foreach(var error in result.Errors)
                 {
