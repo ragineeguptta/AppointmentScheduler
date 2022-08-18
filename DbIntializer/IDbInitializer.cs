c835831a37721660840a35fa3c453002e9c089c3
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-08-18 15:47:53 +0530

    1

---
+++ DbIntializer/IDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.DbIntializer
+{
+    public interface IDbInitializer
+    {
+        void Initalize();
+    }
+}
