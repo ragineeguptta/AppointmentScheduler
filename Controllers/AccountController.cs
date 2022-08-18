88dd3f54f85d6f934323db831de1fb7cba0710d7
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-08-18 12:17:44 +0530

    5

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -79,6 +79,10 @@ namespace AppointmentScheduler.Controllers
                     {
                         await _signInManager.SignInAsync(user, isPersistent: false);
                     }
+                    else
+                    {
+                        TempData["newAdminSignUp"] = user.Name;
+                    }
                     return RedirectToAction("Index", "Appointment");
                 }
                 foreach(var error in result.Errors)
